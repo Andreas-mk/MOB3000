c8cb4781e1f114b510e9f7301e7de3b4652d7edb
Thomas Kirkeng <113349698+ThomasKirkeng@users.noreply.github.com>
2023-10-03 12:17:02 +0200

    Lagd API kall til Fylker, lagd nye klasser som "FylkeAPI og Fylke"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,4 +73,8 @@ dependencies {
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // Implementerer retrofit sine dependencies
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
